7add764f12ade9b7249aca42c5cfa1421c348e74
Muhammad Shamsul Maruf <shamsul.maruf@ideascale.com>
2017-08-03 16:13:47 +0600

    Update to current inital tutorial-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {UserService} from "./service/user.service";
+import {User} from "./model/user.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  users:User[] = [];
+  currentUser:User;
+
+  constructor(private userService: UserService) {
+    this.users = userService.getUsers();
+  }
+
+  showUserDetails(user: User) {
+    this.currentUser = user;
+  }
+
+  saveUser(name: HTMLInputElement, phone: HTMLInputElement) {
+    this.userService.addUser(name.value, phone.value);
+  }
 }
